167423ee62ba1554992cd2cc33b452b4fabe2e01
Francisco C J Bispo <fcjbispo@gmail.com>
2025-03-26 22:15:54 -0300

    Removeu conteudo invalido de fbpyutils_finance

--- fbpyutils_finance/providers/__init__.py
+++
@@ -1,9 +0,0 @@
-from fbpyutils import file as F
-
-_dirname = F.os.path.dirname(__file__)
-
-CERTIFICATES = {
-    f.split(F.os.path.sep)[-1].split('.')[0]: f
-    for f in F.find(_dirname, '*.pem')
-}
-
